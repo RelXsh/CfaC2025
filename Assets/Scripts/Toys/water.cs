663552172ea8d29d6e10023ccff745518b13341d
RelXsh <antonstrela@icloud.com>
2025-03-05 20:32:41 +0300

    0.8 - main story
    - added mian story template
    - modified water task

--- Assets/Scripts/Toys/water.cs
+++ Assets/Scripts/Toys/water.cs
@@ -1,14 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class water : MonoBehaviour
 {
+    public static event Action OnWash;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Entered Water");
+            OnWash?.Invoke();
         }
     }
 }
